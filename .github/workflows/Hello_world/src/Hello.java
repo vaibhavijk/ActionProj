812abd51ef23b569edd94d58d47d93bc77ced762
vaibhavijk <108233636+vaibhavijk@users.noreply.github.com>
2022-09-29 16:25:47 +0530

    Add files via upload

---
+++ .github/workflows/Hello_world/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello
+{
+
+	public static void main(String[] args)
+{
+   System.out.println("Hello world");
+   System.out.println("Hi sunshine, its our 1st java program");
+}
+
+}
